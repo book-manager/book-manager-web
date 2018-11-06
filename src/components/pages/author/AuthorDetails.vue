852af72942e5d178c64de7a84f8efe6070879b8c
Karol Kozakowski <karol.kozakowski@artificial.io>
2018-11-06 21:07:35 +0100

    Move AuthorDetails to IView

--- src/components/pages/author/AuthorDetails.vue
+++ src/components/pages/author/AuthorDetails.vue
@@ -1,6 +1,5 @@
 <template>
-  <section>
-      <el-row :gutter="10" class="author-details">
+      <!-- <el-row :gutter="10" class="author-details">
           <el-col :xs="24" :lg="12" :xl="12"><img :src="authorDetails.avatar_url"/></el-col>
           <el-col :xs="24" :lg="12" :xl="12">
             <div>
@@ -29,8 +28,23 @@
               </template>
           </el-table-column>
           </el-table>
-      </div>
-  </section>
+      </div> -->
+<section>
+  <Row>
+    <Col :lg="6" :xs="24">
+      <img :src="authorDetails.avatar_url"/>
+    </Col>
+    <Col :lg="12" :xs="24" offset="-2">
+        <Button v-if="!authorOwned" type="primary" @click="addAuthor" icon="md-add" ghost>Add as owned</Button>
+        <Tag v-if="authorOwned" type="dot" color="success">Owned</Tag>
+        <Button type="error" icon="ios-heart" ghost>Add as favourite</Button>
+
+        <p class="author-name">{{ authorDetails.name }} {{ authorDetails.surname }}</p>
+        <p class="description"> {{ authorDetails.description }}</p>
+    </Col>
+  </Row>
+  <Table class="books" :columns="headers" :data="authorDetails.books" @on-row-click="showBook"></Table>
+</section>
 </template>
 
 <script>
@@ -40,7 +54,13 @@ export default {
   data () {
     return {
       search: '',
-      authorFavourite: false
+      authorFavourite: false,
+      headers: [
+        {
+          title: 'Title',
+          key: 'title'
+        }
+      ]
     };
   },
   computed: {
@@ -63,8 +83,8 @@ export default {
       this.$store
         .dispatch('createOwnership', { author_id: this.$route.params.id });
     },
-    showBook (row, event, column) {
-      this.$router.push({ path: `/books/${row.id}` });
+    showBook (selection, row) {
+      this.$router.push({ path: `/books/${selection.id}` });
     }
   }
 };
@@ -82,11 +102,11 @@ export default {
   }
 
   .description {
-    font-size: 1.5em;
+    font-size: 1em;
     white-space: pre-wrap;
-    word-wrap: break-word;
-    font-family: inherit;
     line-height: 1.5em;
+    max-width: 70%;
+    margin: auto;
   }
 
   .books {
